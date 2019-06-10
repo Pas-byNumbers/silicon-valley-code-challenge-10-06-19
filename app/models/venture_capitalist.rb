9b09f2aa1ebc3a7c5f2341e73317acaf53d64eeb
Pascal Rassaby <pasbynumbers@gmail.com>
2019-06-10 10:40:17 +0100

    seeds and basic template done

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,28 @@
 class VentureCapitalist
 
+    attr_accessor(:name, :total_worth)
+    attr_reader()
+    attr_writer()
+
+    @@all = []
+
+    def initialize(name:, total_worth:)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+#     VentureCapitalist#name
+# returns a string that is the venture capitalist's name
+# VentureCapitalist#total_worth
+# returns a number that is the total worth of this investor (e.g., think of it as how much money they have)
+# VentureCapitalist.all
+# returns an array of all venture capitalists
+# VentureCapitalist.tres_commas_club
+# returns an array of all venture capitalists in the Trés Commas club (they have more then 1,000,000,000 total_worth)
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,24 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+start1 = Startup.new(name: "Jack-in-a-box", founder: "Robert Owen", domain: "www.jiab.com")
+start2 = Startup.new(name: "Lakeside Clothes", founder: "Patricia Power", domain: "www.lakeside.com")
+start3 = Startup.new(name: "Boris Johnson Jokes", founder: "Boris Johnson", domain: "www.bjjftw.com")
+start4 = Startup.new(name: "Not my Denim", founder: "Billie Jean", domain: "www.notdenim.com")
+start5 = Startup.new(name: "Guns and Moses", founder: "The Pope", domain: "www.ghettochurch.com")
+vent1 =  VentureCapitalist.new(name: "Bruce Wayne", total_worth: 29000000)
+vent2 =  VentureCapitalist.new(name: "Oliver Queen", total_worth: 450000)
+vent3 =  VentureCapitalist.new(name: "Susan Boyle", total_worth: 35000000)
+vent4 =  VentureCapitalist.new(name: "Matt Bellamy", total_worth: 75000000)
+vent5 =  VentureCapitalist.new(name: "Hermione Granger", total_worth: 5000000)
+fund1 = FundingRound.new(startup: start1, venture_capitalist: vent1, type: "Angel", investment: 56000.00)
+fund2 = FundingRound.new(startup: start3, venture_capitalist: vent3, type: "Series B", investment: 28000.00)
+fund3 = FundingRound.new(startup: start1, venture_capitalist: vent2, type: "Series A", investment: 900000.00)
+fund4 = FundingRound.new(startup: start4, venture_capitalist: vent5, type: "Pre-Seed", investment: 2400.00)
+fund5 = FundingRound.new(startup: start5, venture_capitalist: vent4, type: "Seed", investment: 403000.00)
+fund6 = FundingRound.new(startup: start2, venture_capitalist: vent2, type: "Series C", investment: 246000.00)
+fund7 = FundingRound.new(startup: start4, venture_capitalist: vent3, type: "Seed", investment: 75000.00)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
